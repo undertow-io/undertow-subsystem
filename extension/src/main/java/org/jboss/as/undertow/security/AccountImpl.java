a338ab1a50bba7e737cc6549eed6e99de9f64666
Darran Lofthouse <darran.lofthouse@jboss.com>
2013-03-21 16:13:33 +0000

    Added a new NotificationReceiver to handle SecurityNotifications and record audit events accordingly.

--- extension/src/main/java/org/jboss/as/undertow/security/AccountImpl.java
+++ extension/src/main/java/org/jboss/as/undertow/security/AccountImpl.java
@@ -1,5 +1,3 @@
-package org.jboss.as.undertow.security;
-
 /*
  * JBoss, Home of Professional Open Source.
  * Copyright 2013, Red Hat, Inc., and individual contributors
@@ -21,6 +19,8 @@ package org.jboss.as.undertow.security;
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+
+package org.jboss.as.undertow.security;
 
 import java.io.Serializable;
 import java.security.Principal;
